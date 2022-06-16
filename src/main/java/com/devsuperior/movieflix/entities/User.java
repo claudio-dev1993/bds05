af43471eccdfb5b3996445b3dbea211ac0d49f5f
claudio-dev1993 <claudinhoroberto2008@gmail.com>
2022-06-15 21:56:52 -0300

    Authorization and Resource Servers, Web Security Configs

--- src/main/java/com/devsuperior/movieflix/entities/User.java
+++ src/main/java/com/devsuperior/movieflix/entities/User.java
@@ -1,11 +1,12 @@
 package com.devsuperior.movieflix.entities;
 
-import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -18,9 +19,13 @@ import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable {
+public class User implements  UserDetails {
     private static final long serialVersionUID = 1L;
 
     @Id
@@ -106,5 +111,45 @@ public class User implements Serializable {
         User other = (User) obj;
         return Objects.equals(id, other.id);
     }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        
+        return roles.stream()
+        .map(role -> new SimpleGrantedAuthority(role.getAuthority()))
+        .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        
+        return name;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+       
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        
+        return true;
+    }
+
+    
 
 }
